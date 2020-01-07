4c3a62d487d7b666e1929555943b125d67ba9e4f
Ania <57544026+NiecnyKarzel@users.noreply.github.com>
2020-01-07 22:04:17 +0100

    Update calls.py

--- calls.py
+++ calls.py
@@ -1,5 +1,5 @@
-from STM import get_word, text_to_binary_morse, use_dictation
-from MTS import audio_to_text, speech_to_text, load_dane, morse_to_audio, binary_Morse_to_text
+from STM import get_word, text_to_binary_morse
+from MTS import audio_to_text, speech_to_text, load_dane, morse_to_audio, binary_Morse_to_text, morse_to_text
 from micro_input import record_sound_on_key, WAVE_OUTPUT_FILENAME
 import winsound
 import time
@@ -18,10 +18,7 @@ def call_STM():
             break
         elif choice == 2:
             word = input("Wprowadź słowo do przetłumaczenia: ")
-            break
-        elif choice == 3:
-            filepath = input("Podaj ścieżkę do pliku wave: ")
-            word = use_dictation(file_path=filepath)
+
             break
         elif choice == 0:
             return
@@ -29,6 +26,7 @@ def call_STM():
             print("Wybierz prawidłową opcję.\n")
 
     wordlist, wordall = text_to_binary_morse(word)
+    print("Tłumaczenie: ",wordall)
     morse_to_audio(wordlist)
 
     time.sleep(2.5)
@@ -42,11 +40,11 @@ def call_MTS():
 
     choice = 5
     while choice != 0:
-        choice = int(input("Wybierz sposób wprowadzenia sygnału: \n[1] Mowa (jednotonowa samogłoska) \n[2] Inne (maszynowe)\n[3] Sygnał z pliku (.wav) \n\n[0] Cofnij do menu\n"))
+        choice = int(input("Wybierz sposób wprowadzenia sygnału: \n[1] Mowa (jednotonowa samogłoska) \n[2] Inne (maszynowe) \n[3] Klawiatura \n\n[0] Cofnij do menu\n"))
         if choice == 1:
             print("Naciśnij i przytrzymaj LCtrl aby nagrywać. Puść LCtrl, aby zakończyć nagrywanie.\n")
             record_sound_on_key()
-            audio, rate = load_dane(file_name="output\\output.wav")
+            audio,rate = load_dane(file_name="output\\output.wav")
             word = speech_to_text(audio=audio)
             translated = binary_Morse_to_text(word)
 
@@ -92,32 +90,12 @@ def call_MTS():
                     call_synthesize(address, input_text, output_wave_file, sampling_rate)
                     break
             break
-
         elif choice == 3:
-            filename = input("Podaj ścieżkę do pliku wave z sygnałem: ")
-            audio, rate = load_dane(file_name=filename)
-            word = speech_to_text(audio=audio)
-            translated = binary_Morse_to_text(word)
-
-            choice1 = 6
-            while choice1 != 0:
-                choice1 = int(input("Wybierz sposób wyprowadzenia sygnału: \n[1] Wyświetl \n[2] Odtwórz\n"))
-                if choice1 == 1:
-                    print(translated)
-                    break
-                elif choice1 == 2:
-                    output_wave_file = 'tts_output.wav'
-                    ap = AddressProvider()
-                    address = ap.get("tts")
-                    sampling_rate = 44100
-                    input_text = translated
-                    time.sleep(1.5)
-                    call_synthesize(address, input_text, output_wave_file, sampling_rate)
-                    break
-                else:
-                    print("Wybierz prawidłową opcję.\n")
+            print("Zasady: \n1. Słowa oddzielamy spacją \n2. Znaki w literze oddzielamy symbolem |\nPrzykład: |111|000|111| = SOS")
+            word = input("Wprowadź słowo do przetłumaczenia: ")
+            word_Morse=morse_to_text(word)
+            print("Tłumaczenie: ", word_Morse)
             break
-
         elif choice == 0:
             return
         else:
